b51dc8b5f927e9a0211009307026c0b9da2e3116
AlexsanderGS <alexsanderga@gmail.com>
2026-01-09 16:34:55 -0300

    Aula 01: variaveis e tipos de dados - Criando variaveis e atualizando valores.

---
+++ aula-01-variaveis-e-tipos/variaveis.js
@@ -0,0 +1,19 @@
+
+/* Declara a variável IDADE e atribui (=) um valor. */
+idade = 29;
+
+/* Chama o console.log para passar uma mensagem e devolver uma resposta no terminal. */
+/* A mensagem que deverá ser exibida no terminal é: Minha idade atual é: 29 */
+console.log("Minha idade atual é: ", idade);
+
+/* Declara uma nova variável no padrão CamelCase (não se usa acento, espaço ou caracteres especiais em variável). E atribui um valor. */
+dataAniversario = '10 de outubro';
+
+/* Podemos atualizar o valor da variável idade, atribuindo um novo valor */
+idade = 30;
+
+/* Novamente passa uma mensagem ao console.log e exibe no terminal */
+/* A mensagem que deverá ser exibida no terminal é: Minha idade após o aniversário é: 30 */
+console.log("Minha idade após o aniversário é: ", idade);
+
+
