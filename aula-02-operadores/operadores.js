926c8074238a118bd501a620e16691ec11ebe2d6
AlexsanderGS <alexsanderga@gmail.com>
2026-01-21 14:59:36 -0300

    Aula 02: Operadores - Operadores Aritiméticos.

---
+++ aula-02-operadores/operadores.js
@@ -0,0 +1,61 @@
+/* Declaramos duas variáveis como Const e atribuimos valores */
+const numeroUm = 4;
+const numeroDois = 8;
+/* Chamamos o console log para exibir na tela o resultado da subtração. Executamos direto no console log o cálculo. */
+console.log("Subtração dos números um e dois: ", numeroUm - numeroDois);
+
+/* Declaramos uma variavel chamada subtração e atribuimos o calculo de subtração. */
+const subtracao = numeroUm - numeroDois;
+/* Chamamos o console log para exibir na tela o resultado. Passando apenas o resultado da variavel subtração. */
+console.log("Subtração dos números um e dois: ", subtracao);
+
+/* Declaramos uma variavel chamada soma e atribuimos o calculo de soma. */
+const soma = numeroUm + numeroDois;
+/* Chamamos o console log para exibir na tela o resultado. Passando apenas o resultado da variavel soma. */
+console.log("Soma dos números um e dois: ", soma);
+
+/* Declaramos uma variavel chamada divisão e atribuimos o calculo de divisão. */
+const divisao = numeroUm / numeroDois;
+/* Chamamos o console log para exibir na tela o resultado. Passando apenas o resultado da variavel divisão. */
+console.log("Divisão dos números um e dois: ", divisao);
+
+/* Declaramos uma variavel chamada multiplicação e atribuimos o calculo de multiplicação. */
+const multiplicacao = numeroUm * numeroDois;
+/* Chamamos o console log para exibir na tela o resultado. Passando apenas o resultado da variavel multiplicação. */
+console.log("Multiplicação dos números um e dois: ", multiplicacao);
+
+/* Declaramos uma variavel chamada contaCompleta e atribuimos o calculo de uma equação. */
+const contaComplexa = (numeroUm + numeroDois) * numeroUm;
+/* Chamamos o console log para exibir na tela o resultado. Passando apenas o resultado da variavel contaComplexa. */
+console.log(
+  "Resultado da conta complexa dos números um e dois: ",
+  contaComplexa
+);
+
+/* Declaramos uma variável chamada resto, atribuindo dois valores e usando '%' para declarar um cálculo de resto de divisão. */
+const resto = 12 % 2;
+/* Chamamos o console log para exibir na tela o resultado. Passando apenas o resultado da variavel resto. */
+console.log("Resto da divisão de 12 / 2: ", resto);
+
+/* Declaramos uma variável chamada potencia, atribuindo dois valores e usando dois '**' para declarar um cálculo de potência. */
+const potencia = 2 ** 3;
+/* Chamamos o console log para exibir na tela o resultado. Passando apenas o resultado da variavel potência. */
+console.log("Resultado do cálculo de potência de 2^3: ", potencia);
+
+/* Declaramos uma variável chamada contador, atribuindo um valor. */
+let contador = 5;
+/* Atribuimos '+ 1' a variável contador, indicando que deverá somar mais um número a ela. Uma das formas de realizar essa operação.*/
+contador = 5 + 1;
+/* Chamamos o console log para exibir na tela o resultado. Passando apenas o resultado da variavel contador. */
+console.log("Resultado do contador: ", contador);
+/* Atribuimos dois operadores '++' a variável contador, indicando que deverá somar mais um número a ela. Segunda forma de realizar essa operação.*/
+contador++;
+console.log("Resultado do contador: ", contador);
+
+/* Declaramos uma variável chamada numeroTres e atribuimos o valor 3. */
+let numeroTres = 3;
+/* Atribuimos a variável numeroTres a operação '+= 3'. Indicando que o cálcula a ser feito é a variável numeroTres + numeroTres + 3.
+   Esse tipo de operação pode ser realizada com todos os operados: '+= 3', '-= 3, '*= 3' e '/= 3'. */
+numeroTres += 3;
+/* Chamamos o console log para exibir na tela o resultado. Passando apenas o resultado da variavel numeroTres. */
+console.log("Resultado da operação da variável numeroTres: ", numeroTres);
