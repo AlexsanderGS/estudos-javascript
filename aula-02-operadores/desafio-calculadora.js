e37d02848022b414e2bd8beeafd39818ccfe790c
AlexsanderGS <alexsanderga@gmail.com>
2026-01-21 17:19:01 -0300

    Aula 02: Operadores - Realizando o desafio da Calculadora de Trocos.

---
+++ aula-02-operadores/desafio-calculadora.js
@@ -0,0 +1,13 @@
+console.log("Bem vinda a nossa calculadora de troco! Faça sua compra!");
+/* Declaramos as variaveis valorDaCompra e ValorPago e declaramos dois valores number. */
+let valorDaCompra = 27.5;
+let valorPago = 50;
+
+/* Chamamos o console log para exibir na tela o valor da compra e o valor pago. */
+console.log("Valor da compra: R$ ", valorDaCompra);
+console.log("Valor pago: R$ ", valorPago);
+
+/* Declaramos a variavel troco e utlizamos o operador aritmetico '-' para calcular o troco.. */
+let troco = valorPago - valorDaCompra;
+/* Chamamos o console log para exibir na tela o valor da compra e o valor pago. */
+console.log("Troco a receber: R$ ", troco);
